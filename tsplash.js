1a73011b7cb9fa1ebc61d7ad8dd805e0eb589767
dev0ps221 <seybaxscorpion@gmail.com>
2022-02-25 06:44:00 +0000

    Splash!! base ... styles and main elem... Splash

--- tsplash.js
+++ tsplash.js
@@ -1,16 +1,27 @@
 class TeeSplash{
 
 
-
-
-
-
     splashStyle = null
     splash = null
-    styles = []
+    styles = [
+        ['position','absolute'],
+        ['top','0'],
+        ['left','0'],
+        ['width','100vw'],
+        ['height','100vh'],
+        ['background','red']
+    ]
+    _splash_type = 'logo' 
 
 
+    Splash(){
+        this.assignSplashElemStyles()
+        document.body.appendChild(this.splash)
+    }
 
+    UnSplash(){
+        document.body.removeChild(this.splash)
+    }
 
     setStyleAttr(attr,val){
         this.styles.push(
@@ -20,13 +31,14 @@ class TeeSplash{
     }
 
     assignSplashElemStyles(){
-        this.splashStyle.innerText = ``
+        this.splashStyle.innerText = `.TeeSplash{`
         this.styles.forEach(
             style=>{
-                this.splashStyle.innerText+=`${this.splashStyle.innerText}${style[0]}:${style[1]};\n\t`
+                this.splashStyle.innerText =`${this.splashStyle.innerText}${style[0]}:${style[1]};\n\t`
             }
         )
-        this.splash.style = this.splashStyle
+        this.splashStyle.innerText += `}`
+        this.splash.insertBefore(this.splashStyle,this.splash.firstChild)
     }
 
     createSplashElemStyle(){
@@ -36,7 +48,7 @@ class TeeSplash{
 
     createSplashElem(){
         this.splash = document.createElement('section')
-        this.splash.classList('TeeSplash')
+        this.splash.classList.add('TeeSplash')
         this.createSplashElemStyle()
     }
 
