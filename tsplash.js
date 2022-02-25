63c269782c93a6d0140a67fc64071148fbf9a217
dev0ps221 <seybaxscorpion@gmail.com>
2022-02-25 08:09:56 +0000

    Splash!! base ... styles and main elem... howto!!

--- tsplash.js
+++ tsplash.js
@@ -9,14 +9,35 @@ class TeeSplash{
         ['left','0'],
         ['width','100vw'],
         ['height','100vh'],
-        ['background','red']
+        ['display','flex'],
+        ['flex-direction','column'],
+        ['justify-content','center'],
+        ['text-align','center']
+    ]
+    logoStyles = [
+        ['display','flex'],
+        ['width','10vw'],
+        ['height','20vh'],
+        ['background','yellow'],
+        ['border-radius','10em'],
+        ['margin-left','45%']
     ]
     _splash_type = 'logo' 
 
 
+    configure(){
+        if(this._splash_type === 'logo'){
+            this.splashLogo = this.splashLogo && (typeof this.splashLogo != 'undefined') ? this.splashLogo : this.createSplashLogo()
+            this.splashLogoStyle = this.splashLogoStyle ? this.splashLogoStyle : this.createSplashLogoStyle() 
+        }
+    }
+
     Splash(){
         this.assignSplashElemStyles()
-        document.body.appendChild(this.splash)
+        if(this._splash_type=='logo'){
+            this.splash.appendChild(this.splashLogo)
+            document.body.appendChild(this.splash)
+        }
     }
 
     UnSplash(){
@@ -30,7 +51,15 @@ class TeeSplash{
         this.assignSplashElemStyles()
     }
 
+    setLogoStyleAttr(attr,val){
+        this.styles.push(
+            [attr,val]
+        )
+        this.assignSplashElemStyles()
+    }
+
     assignSplashElemStyles(){
+        this.configure()
         this.splashStyle.innerText = `.TeeSplash{`
         this.styles.forEach(
             style=>{
@@ -38,18 +67,48 @@ class TeeSplash{
             }
         )
         this.splashStyle.innerText += `}`
+        if(this._splash_type=='logo'){
+            this.assignSplashLogoStyles()
+        }
         this.splash.insertBefore(this.splashStyle,this.splash.firstChild)
+    }
+
+    assignSplashLogoStyles(){
+        this.configure()
+        this.splashLogoStyle.innerText = `.TeeSplashLogo{`
+        this.logoStyles.forEach(
+            style=>{
+                this.splashLogoStyle.innerText =`${this.splashLogoStyle.innerText}${style[0]}:${style[1]};\n\t`
+            }
+        )
+        this.splashLogoStyle.innerText += `}`
+        this.splash.insertBefore(this.splashLogoStyle,this.splash.firstChild)
     }
 
     createSplashElemStyle(){
         this.splashStyle = document.createElement('style')
         this.assignSplashElemStyles()
+        return this.splashStyle
+    }
+
+    createSplashLogoStyle(){
+        this.splashLogoStyle = document.createElement('style')
+        this.assignSplashLogoStyles()
+        return this.splashLogoStyle
     }
 
     createSplashElem(){
         this.splash = document.createElement('section')
         this.splash.classList.add('TeeSplash')
         this.createSplashElemStyle()
+        return this.splash
+    }
+
+    createSplashLogo(){
+        this.splashLogo = document.createElement('img')
+        this.splashLogo.classList.add('TeeSplashLogo')
+        this.createSplashLogoStyle()
+        return this.splashLogo
     }
 
 
